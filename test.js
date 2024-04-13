9779712d27db156e4e7eceecf659714205a1aa9d
Bend <144515341+APIDT@users.noreply.github.com>
2024-04-13 09:35:30 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,34 @@
+const prompt = require("prompt-sync")()
+const bcrypt = require('bcrypt');
+
+console.log("no fewer than 9 characters")
+console.log("==================================")
+const password = prompt("Enter :")
+
+function checkPass(password){
+  if(password.length > 8){
+    return true
+  }
+}
+
+async function hashPassword(Password){
+  switch(checkPass(password)){
+    case true:
+      const hesh = await bcrypt.hash(Password, 5)
+      console.log("Hash Password: " + hesh)
+      checkHash(password, hesh)
+      break
+    default: console.log("no fewer than 9 characters")
+  }
+}
+
+async function checkHash(x, y){
+const ch = await bcrypt.compare(x, y)
+switch(ch){
+  case true : console.log("success");
+  break;
+  default: console.log("Error")
+} 
+}
+
+hashPassword(password)
